1bb4bfabba3e91054ef86f371ed38b16cc6d8df9
assma <dimah.assma@gmail.com>
2020-03-12 10:27:42 +0100

    petite modif

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -30,7 +30,7 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="
-                    {{-- {{route('image')}} --}}
+                     {{route('image')}}
                     ">Images</a>
                 </li>
             </ul>
